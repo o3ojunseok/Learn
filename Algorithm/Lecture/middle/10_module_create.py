fa90a3b006c4e0837113272e23b4d20f74ffd2b4
junseok <o3ojunseok@gmail.com>
2023-02-17 08:43:50 +0900

    create_module

---
+++ Algorithm/Lecture/middle/10_module_create.py
@@ -0,0 +1,13 @@
+# 특정 기능을 가진 파이썬 파일
+def add(x, y):
+  print(f'덧셈: {x + y}')
+def sub(x, y):
+  print(f'뺄셈: {x - y}')
+def mul(x, y):
+  print(f'곱셈: {x * y}')
+def div(x, y):
+  print(f'나눗셈: {x / y}')
+
+
+# 파일 하나 더 만들어서 import 기능가진파일명
+
