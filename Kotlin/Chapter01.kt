1e892dbeb5d5ab4862543f1a052ea87e3da0f676
o3ojunseok <o3ojunseok@gmail.com>
2024-04-28 02:17:30 +0900

    Feat: chapter01 kotlin

---
+++ Kotlin/Chapter01.kt
@@ -0,0 +1,20 @@
+fun main(){
+  println(sum(10, 20))
+ println(sum2(10, 20))
+ printSum(10, 20)
+ printSumNoUnit(10, 20)
+
+ println("1과 10중 최댓값은 ${maxOf(1,2)}입니다.");
+ println("1과 100중 최댓값은 ${maxf2(1,2)}입니다.");
+
+ printProduct("1", "9");
+ printProduct("a", "2");
+ printProduct("a", "b");
+
+ printProduct2("1", "9");
+ printProduct2("a", "4");
+ printProduct2("a", "b");
+ 
+ //string_template
+ string_template()
+}
